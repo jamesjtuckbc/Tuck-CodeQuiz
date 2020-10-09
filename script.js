2bb3d5971ddf6aa4c391d9e54249613b0c3cc0f8
James Tuck <JamesjTuck@gmail.com>
2020-10-09 15:30:32 -0600

    added array for questions with placeholder text and started newQuestion function

--- script.js
+++ script.js
@@ -6,7 +6,99 @@ var timerEl = document.querySelector("#timer");
 var newDiv = document.createElement('div');
 var newH1 = document.createElement('h1');
 var newP = document.createElement('p');
+var newBtn = document.createElement('button');
 var highscoreBool = false;
+
+
+
+
+// questions array
+var questions = [
+    {
+        question: "question 1",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer2",
+    },
+    {
+        question: "question 2",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer3",
+    },
+    {
+        question: "question 3",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer4",
+    },
+    {
+        question: "question 4",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer1",
+    },
+    {
+        question: "question 5",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer2",
+    },
+    {
+        question: "question 6",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer3",
+    },
+    {
+        question: "question 7",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer4",
+    },
+    {
+        question: "question 8",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer1",
+    },
+    {
+        question: "question 9",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer2",
+    },
+    {
+        question: "question 10",
+        answer1: "answer 1",
+        answer2: "answer 2",
+        answer3: "answer 3",
+        answer4: "answer 4",
+        correctAnswer: "answer3",
+    }
+]
+
+
+
+// FUNCTIONS
 
 
 // function to create first page
@@ -21,8 +113,10 @@ function initialize() {
     newDiv.appendChild(newH1);
     
     newP.setAttribute("class","centered lead");
-    newP.textContent = "Code Quiz";
+    newP.textContent = "This is a fun quiz on coding!";
     newH1.appendChild(newP);
+
+    newBtn.setAttribute();
 };
 
 // function to creat highscore page
@@ -58,10 +152,18 @@ function switchPage(event) {
     
 };
 
+// function to clear child nodes i.e. when switching pages
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+};
+
+// function to create question and answer buttons
+function newQuestion(){
+    for(var i = 0; i < questions.length; i++){
+
+    };
 };
 
 // Event listeners
