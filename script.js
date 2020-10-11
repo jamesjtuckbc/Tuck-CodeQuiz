080ed1569a59316213aed784ead510bbc312b634
James Tuck <JamesjTuck@gmail.com>
2020-10-10 18:32:07 -0600

    replaced question placeholder text with real questions

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var mainContentEl = document.querySelector("#mainContent");
 var highScoreEl = document.querySelector("#highscores");
 var timerEl = document.querySelector("#timer");
 var newDiv = document.createElement("div");
+var btnDiv = document.createElement("div");
+var answer1DivRow = document.createElement("div");
+var answer2DivRow = document.createElement("div");
+var answer3DivRow = document.createElement("div");
+var answer4DivRow = document.createElement("div");
+var answer1DivCol = document.createElement("div");
+var answer2DivCol = document.createElement("div");
+var answer3DivCol = document.createElement("div");
+var answer4DivCol = document.createElement("div");
 var newH1 = document.createElement("h1");
 var newP = document.createElement("p");
 var newBtn = document.createElement("button");
@@ -29,83 +38,83 @@ var htmlObj = {
 // questions array
 var questions = [
     {
-        question: "question 1",
-        answer1: "answer 1",
-        answer2: "answer 2",
-        answer3: "answer 3",
-        answer4: "answer 4",
-        correctAnswer: "answer2",
-    },
-    {
-        question: "question 2",
-        answer1: "q2answer 1",
-        answer2: "q2answer 2",
-        answer3: "q2answer 3",
-        answer4: "q2answer 4",
-        correctAnswer: "answer3",
-    },
-    {
-        question: "question 3",
-        answer1: "q3answer 1",
-        answer2: "q3answer 2",
-        answer3: "q3answer 3",
-        answer4: "q3answer 4",
+        question: "Which of the following is an advantage of using JavaScript?",
+        answer1: "Less server interaction",
+        answer2: "Immediate feedback to the visitors",
+        answer3: "Increased interactivity",
+        answer4: "All of the above.",
         correctAnswer: "answer4",
     },
     {
-        question: "question 4",
-        answer1: "q4answer 1",
-        answer2: "q4answer 2",
-        answer3: "q4answer 3",
-        answer4: "q4answer 4",
-        correctAnswer: "answer1",
-    },
-    {
-        question: "question 5",
-        answer1: "q5answer 1",
-        answer2: "q5answer 2",
-        answer3: "q5answer 3",
-        answer4: "q5answer 4",
-        correctAnswer: "answer2",
-    },
-    {
-        question: "question 6",
-        answer1: "q6answer 1",
-        answer2: "q6answer 2",
-        answer3: "q6answer 3",
-        answer4: "q6answer 4",
+        question: "Which built-in method returns the character at the specified index?",
+        answer1: "characterAt()",
+        answer2: "getCharAt()",
+        answer3: "charAt()",
+        answer4: "None of the above.",
         correctAnswer: "answer3",
     },
     {
-        question: "question 7",
-        answer1: "q7nswer 1",
-        answer2: "q7nswer 2",
-        answer3: "q7nswer 3",
-        answer4: "q7nswer 4",
+        question: "JavaScript is a ___ -side programming language.",
+        answer1: "Client",
+        answer2: "Server",
+        answer3: "Both",
+        answer4: "None",
+        correctAnswer: "answer3",
+    },
+    {
+        question: "Which of the following will write the message “Hello DataFlair!” in an alert box?",
+        answer1: "alertBox(“Hello DataFlair!”);",
+        answer2: "alert(Hello DataFlair!);",
+        answer3: "msgAlert(“Hello DataFlair!”);",
+        answer4: "alert(“Hello DataFlair!”);",
         correctAnswer: "answer4",
     },
     {
-        question: "question 8",
-        answer1: "q8answer 1",
-        answer2: "q8answer 2",
-        answer3: "q8answer 3",
-        answer4: "q8answer 4",
-        correctAnswer: "answer1",
-    },
-    {
-        question: "question 9",
-        answer1: "q9answer 1",
-        answer2: "q9answer 2",
-        answer3: "q9answer 3",
-        answer4: "q9answer 4",
+        question: "How do you find the minimum of x and y using JavaScript?",
+        answer1: "min(x,y);",
+        answer2: "Math.min(x,y)",
+        answer3: "Math.min(xy)",
+        answer4: "min(xy);",
         correctAnswer: "answer2",
     },
     {
-        question: "question 10",
-        answer1: "q10answer 1",
-        answer2: "q10answer 2",
-        answer3: "q10answer 3",
-        answer4: "q10answer 4",
+        question: "Which JavaScript label catches all the values, except for the ones specified?",
+        answer1: "catch",
+        answer2: "label",
+        answer3: "try",
+        answer4: "default",
+        correctAnswer: "answer4",
+    },
+    {
+        question: "Which are the correct \"if\" statements to execute certain code if “x” is equal to 2?",
+        answer1: "if(x 2)",
+        answer2: "if(x = 2)",
+        answer3: "if(x == 2)",
+        answer4: "if(x != 2 )",
+        correctAnswer: "answer3",
+    },
+    {
+        question: "What is the correct JavaScript syntax to print “DataFlair” in the console?",
+        answer1: "print(“DataFlair”);",
+        answer2: "console.print(“DataFlair”);",
+        answer3: "log.console(“DataFlair”);",
+        answer4: "console.log(“DataFlair”);",
+        correctAnswer: "answer4",
+    },
+    {
+        question: "Which of the following print content on the browser window?",
+        answer1: "document.write(“print content”);",
+        answer2: "response.write(“print content”);",
+        answer3: "document.write(print content);",
+        answer4: "write(“print content”);",
+        correctAnswer: "answer1",
+    },
+    {
+        question: "Which method will you use to round the number 24.76 to the nearest integer?",
+        answer1: "round(24.76);",
+        answer2: "rnd(24.76);",
+        answer3: "Math.round(24.76);",
+        answer4: "Math.rnd(24.76);",
         correctAnswer: "answer3",
     }
 ]
@@ -115,26 +124,36 @@ var questions = [
 // FUNCTIONS
 
 mainContentEl.appendChild(newDiv);
-newDiv.setAttribute("class", "centered");
-answerBtn1.setAttribute("id", "answer1")
-answerBtn2.setAttribute("id", "answer2")
-answerBtn3.setAttribute("id", "answer3")
-answerBtn4.setAttribute("id", "answer4")
-answerBtn1.setAttribute("class", "answerBtn")
-answerBtn2.setAttribute("class", "answerBtn")
-answerBtn3.setAttribute("class", "answerBtn")
-answerBtn4.setAttribute("class", "answerBtn")
+newDiv.setAttribute("class", "centered container justify-content-md-center");
+answerBtn1.setAttribute("id", "answer1");
+answerBtn2.setAttribute("id", "answer2");
+answerBtn3.setAttribute("id", "answer3");
+answerBtn4.setAttribute("id", "answer4");
+answer1DivRow.setAttribute("class", "row");
+answer2DivRow.setAttribute("class", "row");
+answer3DivRow.setAttribute("class", "row");
+answer4DivRow.setAttribute("class", "row");
+answer1DivCol.setAttribute("class", "col-12");
+answer2DivCol.setAttribute("class", "col-12");
+answer3DivCol.setAttribute("class", "col-12");
+answer4DivCol.setAttribute("class", "col-12");
+btnDiv.setAttribute("class","row justify-content-md-center");
+answerBtn1.setAttribute("class", "answerBtn");
+answerBtn2.setAttribute("class", "answerBtn");
+answerBtn3.setAttribute("class", "answerBtn");
+answerBtn4.setAttribute("class", "answerBtn");
 // function to create first page
 function initialize() {
     clearInterval(timerCountDown);
     timerEl.style.display = "block";
+    newBtn.style.display = "block";
     time = 60;
     newScore = 0;
-    mainContentEl.setAttribute("class", "centered");
+    mainContentEl.setAttribute("class", "centered container");
     newDiv.textContent = "";
     mainContentEl.appendChild(newDiv);
 
-    newDiv.setAttribute("class", "centered");
+    newDiv.setAttribute("class", "centered container justify-content-md-center");
     newH1.setAttribute("class", "centered display-4");
     newH1.textContent = "Code Quiz";
     newDiv.appendChild(newH1);
@@ -142,11 +161,12 @@ function initialize() {
     newP.setAttribute("class", "centered lead");
     newP.textContent = "This is a fun quiz on coding!";
     newH1.appendChild(newP);
+    newH1.appendChild(btnDiv);
 
     newBtn.setAttribute("class", "centered btn");
     newBtn.textContent = "Start Quiz"
     newBtn.setAttribute("id", "startQuiz");
-    newP.appendChild(newBtn);
+    btnDiv.appendChild(newBtn);
     document.querySelector("#startQuiz").addEventListener("click", function () {
         questionNumber = 0;
         newQuestion();
@@ -156,22 +176,31 @@ function initialize() {
 
 // function to create highscore page
 function highScore() {
+    removeAllChildNodes(mainContentEl);
+    highScoreInputEl.value = "";
     timerEl.style.display = "none";
-    mainContentEl.setAttribute("class", "centered");
+    mainContentEl.setAttribute("class", "centered container");
     newDiv.textContent = "";
     mainContentEl.appendChild(newDiv);
-    newDiv.setAttribute("class", "centered");
+    newDiv.setAttribute("class", "centered container justify-content-md-center");
     newH1.setAttribute("class", "centered display-4");
     newH1.textContent = "High Scores";
     newDiv.appendChild(newH1);
 
-    newP.setAttribute("class", "centered lead");
-    var highScoreUser = JSON.parse(localStorage.getItem("user"));
+    // newP.setAttribute("class", "centered lead");
+    var highScoreUser = JSON.parse(localStorage.getItem("allEntries"));
     console.log(highScoreUser);
-    for(var i = 0; i < highScoreUser.length; i++) {
-        newP.textContent = "Initials: " + highScoreUser[i].initials + " - Score: " + highScoreUser[i].score + " - Time Remaining: " + highScoreUser[i].timeRemaining;
+    if (highScoreUser == null) {
+
+    } else {
+        for (var i = 0; i < highScoreUser.length; i++) {
+            var newP = document.createElement("p");
+            newP.setAttribute("class", "centered lead");
+            newP.textContent = "Initials: " + highScoreUser[i].initials + " - Score: " + highScoreUser[i].score + " - Time Remaining: " + highScoreUser[i].timeRemaining;
+            newH1.appendChild(newP);
+        }
     }
-    newH1.appendChild(newP);
+
 
     highScoreEl.textContent = "Code Quiz";
 
@@ -203,6 +232,7 @@ function removeAllChildNodes(parent) {
 // function to create question and answer buttons
 function newQuestion() {
     if (questionNumber < questions.length) {
+        newBtn.style.display = "none";
         newDiv.textContent = questions[questionNumber].question;
 
         answerBtn1.textContent = questions[questionNumber].answer1;
@@ -210,10 +240,18 @@ function newQuestion() {
         answerBtn3.textContent = questions[questionNumber].answer3;
         answerBtn4.textContent = questions[questionNumber].answer4;
 
-        newDiv.append(answerBtn1);
-        newDiv.append(answerBtn2);
-        newDiv.append(answerBtn3);
-        newDiv.append(answerBtn4);
+        newDiv.append(answer1DivRow);
+        newDiv.append(answer2DivRow);
+        newDiv.append(answer3DivRow);
+        newDiv.append(answer4DivRow);
+        answer1DivRow.append(answer1DivCol);
+        answer2DivRow.append(answer2DivCol);
+        answer3DivRow.append(answer3DivCol);
+        answer4DivRow.append(answer4DivCol);
+        answer1DivCol.append(answerBtn1);
+        answer2DivCol.append(answerBtn2);
+        answer3DivCol.append(answerBtn3);
+        answer4DivCol.append(answerBtn4);
     } else {
         clearInterval(timerCountDown);
         newHighScore();
@@ -224,10 +262,11 @@ function newQuestion() {
 // function to record highscore
 function newHighScore() {
     removeAllChildNodes(mainContentEl);
-    mainContentEl.setAttribute("class", "centered");
+    newBtn.style.display = "inline";
+    mainContentEl.setAttribute("class", "centered container");
     newDiv.textContent = "";
     mainContentEl.appendChild(newDiv);
-    newDiv.setAttribute("class", "centered");
+    newDiv.setAttribute("class", "centered container justify-content-md-center");
     newH1.setAttribute("class", "centered display-4");
     newH1.textContent = "High Scores";
     newDiv.appendChild(newH1);
@@ -241,39 +280,43 @@ function newHighScore() {
     highScoreLabelEl.setAttribute("for", "highScoreInput")
     highScoreLabelEl.textContent = "Please enter your initials"
     newP.append(highScoreLabelEl);
-    
+
     highScoreInputEl.setAttribute("placeholder", "JJT")
     highScoreInputEl.setAttribute("id", "highScoreInput")
     highScoreLabelEl.append(highScoreInputEl);
 
     newBtn.textContent = "Submit";
-    newBtn.setAttribute("id","imputSubmit")
-    newP.append(newBtn);
+    newDiv.append(newBtn);
 
-    newBtn.addEventListener("click",function(event) {
+    newBtn.addEventListener("click", function (event) {
         event.preventDefault();
-        var user = [{
+        console.log(highScoreLabelEl.value);
+
+        var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+        if (existingEntries == null) existingEntries = [];
+        var user = {
             initials: highScoreInputEl.value,
             score: newScore,
             timeRemaining: time
-        }];
-        var oldUsers = JSON.parse(localStorage.getItem("user"));
-        
-            user.push(oldUsers);
-            localStorage.setItem("user", JSON.stringify(user));
-     
-        // localStorage.setItem("user", JSON.stringify(user));
+        };
+
+        // Save allEntries back to local storage
+        existingEntries.push(user);
+        localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+
+
         console.log(user);
         highscoreBool = true;
         clearInterval(timerCountDown);
         highScore();
+
     });
 };
 
 // timer function
 function timer() {
-    timerCountDown = setInterval(function(){
-        if(time === 1) {
+    timerCountDown = setInterval(function () {
+        if (time === 1) {
             clearInterval(timerCountDown);
         }
         time--;
@@ -285,12 +328,12 @@ highScoreEl.addEventListener("click", switchPage);
 
 answerBtn1.addEventListener("click", function () {
     if (questions[questionNumber].correctAnswer === "answer1") {
-        newScore+=5
+        newScore += 5
         questionNumber++
         console.log("Correct");
         newQuestion();
     } else {
-        time-=5;
+        time -= 5;
         questionNumber++
         console.log("wrong");
         newQuestion();
@@ -299,12 +342,12 @@ answerBtn1.addEventListener("click", function () {
 });
 answerBtn2.addEventListener("click", function () {
     if (questions[questionNumber].correctAnswer === "answer2") {
-        newScore+=5
+        newScore += 5
         questionNumber++
         console.log("Correct");
         newQuestion();
     } else {
-        time-=5;
+        time -= 5;
         questionNumber++
         console.log("wrong");
         newQuestion();
@@ -312,12 +355,12 @@ answerBtn2.addEventListener("click", function () {
 });
 answerBtn3.addEventListener("click", function () {
     if (questions[questionNumber].correctAnswer === "answer3") {
-        newScore+=5
+        newScore += 5
         questionNumber++
         console.log("Correct");
         newQuestion();
     } else {
-        time-=5;
+        time -= 5;
         questionNumber++
         console.log("wrong");
         newQuestion();
@@ -325,12 +368,12 @@ answerBtn3.addEventListener("click", function () {
 });
 answerBtn4.addEventListener("click", function () {
     if (questions[questionNumber].correctAnswer === "answer4") {
-        newScore+=5
+        newScore += 5
         questionNumber++
         console.log("Correct");
         newQuestion();
     } else {
-        time-=5;
+        time -= 5;
         questionNumber++
         console.log("wrong");
         newQuestion();
